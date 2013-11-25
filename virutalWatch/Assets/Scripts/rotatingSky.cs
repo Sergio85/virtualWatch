0b29e5035cbb9b83c7bff9ea1e167294a471384d
Stephan Leenders <stephanleenders1.0@gmail.com>
2013-11-25 16:54:02 +0100

    first commit
    marmoset folder is in gitignore now .. you can add it to your assets
    folder and it will not be uploadet the next time

---
+++ virutalWatch/Assets/Scripts/rotatingSky.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotatingSky : MonoBehaviour {
+	
+	public mset.Sky targetSky = null;
+	
+	public Vector3 bigScale = new Vector3(1.2f,1.21f,1.2f);
+	public Vector3 hoverScale = new Vector3(1f,1f,1f);
+	public Vector3 littleScale = new Vector3(0.75f,0.76f,0.75f);
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Rotate(0,020f*Time.deltaTime,0);
+		transform.localScale = bigScale;
+		
+	}
+}
---
+++ virutalWatch/Assets/Scripts/rotatingZeiger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotatingZeiger : MonoBehaviour {
+	
+	public GameObject lupenCam = null;
+	public GameObject lupe = null;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0,010f*Time.deltaTime,0);
+	
+		if(lupenCam != null && lupe != null) {
+			
+			lupenCam.transform.position = lupe.transform.position;
+			
+		}
+		
+		
+	}
+}
