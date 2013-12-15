a13391e9e48bb4de0ad9e018fa6fde70a5c42bdc
Installation <pc@alfonso.de>
2013-12-15 11:55:25 +0100

    uhrwerk getausch, lupe, materialien

---
+++ virutalWatch/Assets/Scripts/Helper/Lupe.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lupe : MonoBehaviour {
+
+	public GameObject lupe;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//lupe.transform.rotation = Quaternion.Euler(90, 0, 0);
+		transform.position = lupe.transform.position;
+	}
+}
