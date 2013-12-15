6e5cc13c8bc1c6781c01cd72a774be5f97379c31
Installation <pc@alfonso.de>
2013-12-15 19:31:46 +0100

    Uhrwerk Animation
    Uhrwerk wurde animiert

--- virutalWatch/Assets/Scripts/ClockAnimation/UntweenedGearAnimation.cs
+++ virutalWatch/Assets/Scripts/ClockAnimation/UntweenedGearAnimation.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class UntweenedGearAnimation : MonoBehaviour {
 
+	public const float timeSpread = 10f;
 	public float anglePerSecond = 0.0f;
-	
+	public bool constantTime = true;
 	
 	// Use this for initialization
 	void Start () {
-		
+		if (!constantTime) {
+			anglePerSecond *= timeSpread;
+		}
+
 	}
 	
 	// Update is called once per frame
