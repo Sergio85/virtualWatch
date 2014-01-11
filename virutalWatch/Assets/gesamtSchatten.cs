f4e5c15e06045851a26a4c8359c0d4302677c9e8
Installation <pc@alfonso.de>
2014-01-11 19:10:39 +0100

    Spiegelung, Scheibe und Schattenbearbeitung

---
+++ virutalWatch/Assets/gesamtSchatten.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gesamtSchatten : MonoBehaviour {
+
+	public RenderTexture schattenTexture;
+	public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		//DepthTextureMode.Depth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//schattenTexture.depth = cam.depth;
+
+	}
+}
